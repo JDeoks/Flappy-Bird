6b1f93fbbe66c959c190d7202a221d07e610dc36
jeongdeok <duginee@naver.com>
2022-12-24 23:46:42 +0900

    bird animation

--- Flappy-Bird/GameScene.swift
+++ Flappy-Bird/GameScene.swift
@@ -9,7 +9,38 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    // 뷰컨에 의해서 scene이 present되었을 때
     override func didMove(to view: SKView) {
+        createBird()
+        createEnviroment()
+    }
+    
+    func createBird() {
+        // 스프라이트 아틀라스
+        let birdTexture = SKTextureAtlas(named: "Bird")
+        
+        let bird = SKSpriteNode(imageNamed: "bird1")
+        bird.position = CGPoint(x:self.size.width / 2, y: self.size.height / 2)
+        bird.zPosition = 2
+        self.addChild(bird)
+        
+    // 애니메이션 부분
+        
+        // 스프라이트 아틀라스 요소 SKTexture배열에 추가
+        var aniArray = [SKTexture]()
+        for i in 1...birdTexture.textureNames.count {
+            aniArray.append(SKTexture(imageNamed: "bird\(i)"))
+        }
+        let flyingAnimation = SKAction.animate(with: aniArray, timePerFrame: 0.1)
+        // 영원히 반복하는 SKAction 만들기
+        let flyingForever = SKAction.repeatForever(flyingAnimation)
+        //bird가 해당 액션 수행하게 함
+        bird.run(flyingForever)
+        
+        
+    }
+    
+    func createEnviroment() {
         // land라는 이미지 노드 생성
         let land = SKSpriteNode(imageNamed: "land")
         // 위치 잡아줌, 여기서 self는 메소드 정의된 클래스 인스턴스 말함
@@ -23,11 +54,6 @@ class GameScene: SKScene {
         sky.position = CGPoint(x:self.size.width / 2, y: 100)
         sky.zPosition = 1
         self.addChild(sky)
-        
-        let bird = SKSpriteNode(imageNamed: "bird")
-        bird.position = CGPoint(x:self.size.width / 2, y: 200)
-        bird.zPosition = 2
-        self.addChild(bird)
         
         let ceiling = SKSpriteNode(imageNamed: "ceiling")
         ceiling.position = CGPoint(x:self.size.width / 2, y: 300)
